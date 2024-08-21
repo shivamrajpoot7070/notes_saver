c9201f61886985e77c6fa2f0fc49d7a915ec5f81
shivamrajpoot7070 <shivamrajpoot7070@gmail.com>
2024-08-22 00:07:50 +0530

    first codst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/Noteslist';
+import Search from './components/Search';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [notes, setNotes] = useState(() => {
+		// Retrieve notes from localStorage or use initial notes
+		const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'));
+		return savedNotes || [
+			{
+				id: nanoid(),
+				text: 'This is my first note!',
+				date: '15/04/2021',
+			},
+			{
+				id: nanoid(),
+				text: 'This is my second note!',
+				date: '21/04/2021',
+			},
+			{
+				id: nanoid(),
+				text: 'This is my third note!',
+				date: '28/04/2021',
+			},
+			{
+				id: nanoid(),
+				text: 'This is my new note!',
+				date: '30/04/2021',
+			},
+		];
+	});
+
+	const [searchText, setSearchText] = useState('');
+	const [darkMode, setDarkMode] = useState(false);
+
+	useEffect(() => {
+		localStorage.setItem('react-notes-app-data', JSON.stringify(notes));
+	}, [notes]);
+
+	const addNote = (text) => {
+		const date = new Date();
+		const newNote = {
+			id: nanoid(),
+			text: text,
+			date: date.toLocaleDateString(),
+		};
+		setNotes((prevNotes) => [...prevNotes, newNote]);
+	};
+
+	const deleteNote = (id) => {
+		setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+	};
+
+	return (
+		<div className={`${darkMode && 'dark-mode'}`}>
+			<div className='container'>
+				<Header handleToggleDarkMode={setDarkMode} />
+				<Search handleSearchNote={setSearchText} />
+				<NotesList
+					notes={notes.filter((note) =>
+						note.text.toLowerCase().includes(searchText.toLowerCase())
+					)}
+					handleAddNote={addNote}
+					handleDeleteNote={deleteNote}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default App;
